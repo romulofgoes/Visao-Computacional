298479d45c39b5894e0f58846e3574406cb2b576
romulofgoes <romulofgoes@proton.me>
2026-05-29 16:33:44 -0300

    reafcotr: fator_mediana - pastas de entrada e saida de imagem e exemplo.py

---
+++ filtro_de_mediana/exemplo.py
@@ -0,0 +1,6 @@
+import cv2
+from filtro_de_mediana import filtro_mediana
+
+imagem_original = cv2.imread("imagem_entrada/Questionario-3-Imagem-3.tif")
+for i in range(3,7+1,2):
+    cv2.imwrite(f"imagem_saida/Questionario-3-imagem-janela-{i}.jpg", filtro_mediana(imagem_original, i))
